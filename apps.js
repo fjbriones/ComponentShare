0fbf707a0df8951c7383f0234752f39040bb0e2e
Ferdinand John Briones <fjbriones@outlook.com>
2019-03-05 07:28:36 +0800

    Modify connection to mysql

--- apps.js
+++ apps.js
@@ -28,28 +28,31 @@ app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/html/login.html');
+	mysql_con.connect(function(err){
+		if(err) throw err;
+	})
 })
 app.post('/login', function (req, res) {
 	// var sql_usrlog = 
-	mysql_con.connect(function(err){
+	// mysql_con.connect(function(err){
+	// 	if(err) throw err;
+	var sql_com_uname = "SELECT pword, profile_id FROM usrlogin WHERE uname=?"
+	mysql_con.query(sql_com_uname, [req.body.username], function(err, result, fields){
 		if(err) throw err;
-		var sql_com_uname = "SELECT pword, profile_id FROM usrlogin WHERE uname=?"
-		mysql_con.query(sql_com_uname, [req.body.username], function(err, result, fields){
-			if(err) throw err;
-			console.log(result[0])
-			console.log(req.body.password)
-			if (result[0].pword == req.body.password)
-			{
-				res.redirect('/home');
-				app.set('profile_id', result[0].profile_id)
-				mysql_con.end();
-			}
-			else
-			{
-				res.redirect('/');
-				mysql_con.end();
-			}
-		})
+		console.log(result[0])
+		console.log(req.body.password)
+		if (result[0].pword == req.body.password)
+		{
+			res.redirect('/home');
+			app.set('profile_id', result[0].profile_id)
+			mysql_con.end();
+		}
+		else
+		{
+			res.redirect('/');
+			mysql_con.end();
+		}
+		// })
 	})
 })
 
@@ -63,28 +66,28 @@ app.get('/signup', function (req, res) {
 })
 
 app.post('/registered', function (req, res) {
-	mysql_con.connect(function(err){
+	// mysql_con.connect(function(err){
+	// 	if(err) throw err;
+	var sql_com_usrprof = "INSERT INTO usrprofiles (fname, lname, email, contactnum) VALUES (?, ?, ?, ?)";
+	var sql_com_usrid = "SELECT profile_id FROM usrprofiles WHERE email=?";
+	var sql_com_usrlog = "INSERT INTO usrlogin (uname, pword, profile_id) VALUES (?, ?, ?)";
+	var prof_id;
+	mysql_con.query(sql_com_usrprof,[req.body.firstname, req.body.lastname, req.body.email, req.body.number], function(err, result){
 		if(err) throw err;
-		var sql_com_usrprof = "INSERT INTO usrprofiles (fname, lname, email, contactnum) VALUES (?, ?, ?, ?)";
-		var sql_com_usrid = "SELECT profile_id FROM usrprofiles WHERE email=?";
-		var sql_com_usrlog = "INSERT INTO usrlogin (uname, pword, profile_id) VALUES (?, ?, ?)";
-		var prof_id;
-		mysql_con.query(sql_com_usrprof,[req.body.firstname, req.body.lastname, req.body.email, req.body.number], function(err, result){
-			if(err) throw err;
-			console.log("1 record inserted into usrprofiles");
-			mysql_con.query(sql_com_usrid, [req.body.email], function (err2, result2, fields){
-				if (err) throw err;
-				prof_id = result2[0].profile_id;
-				mysql_con.query(sql_com_usrlog, [req.body.username, req.body.password, prof_id], function(err3, result3){
-					if(err) throw err;
-					console.log("1 record inserted into usrlogin");
-					mysql_con.end();
-				});
+		console.log("1 record inserted into usrprofiles");
+		mysql_con.query(sql_com_usrid, [req.body.email], function (err2, result2, fields){
+			if (err) throw err;
+			prof_id = result2[0].profile_id;
+			mysql_con.query(sql_com_usrlog, [req.body.username, req.body.password, prof_id], function(err3, result3){
+				if(err) throw err;
+				console.log("1 record inserted into usrlogin");
+				mysql_con.end();
 			});
 		});
-		res.redirect('/');
-	})
+	});
+	res.redirect('/');
 })
+// })
 
 app.get('/addinv', function(req, res) {
 	res.sendFile(__dirname + '/html/addinv.html')
@@ -95,15 +98,15 @@ app.get('/addreq', function(req, res) {
 })
 
 app.post('/addreq', function(req, res) {
-	mysql_con.connect(function(err){
+	// mysql_con.connect(function(err){
+	// 	if(err) throw err;
+	var sql_com_addreq = "INSERT INTO request (r_quantity, r_item, r_remarks, profile_id) VALUES (?, ?, ?, ?)";
+	mysql_con.query(sql_com_addreq, [req.body.quantity, req.body.component, req.body.others, app.get('profile_id')], function(err, result){
 		if(err) throw err;
-		var sql_com_addreq = "INSERT INTO request (r_quantity, r_item, r_remarks, profile_id) VALUES (?, ?, ?, ?)";
-		mysql_con.query(sql_com_addreq, [req.body.quantity, req.body.component, req.body.others, app.get('profile_id')], function(err, result){
-			if(err) throw err;
-			console.log("1 record inserted into request for ", app.get("profile_id"));
-			res.redirect('/home')
-		}) 
-	})
+		console.log("1 record inserted into request for ", app.get("profile_id"));
+		res.redirect('/home')
+	}) 
 })
+// })
 
 app.listen(port);
