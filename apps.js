d492b6b855b9e1a7c0acff6106a4fb5d102add0e
Ferdinand John Briones <fjbriones@outlook.com>
2019-04-19 16:20:47 +0800

    Added matching algorithm and matches table

--- apps.js
+++ apps.js
@@ -11,7 +11,7 @@ var components;
 
 const hostname = '127.0.0.1';
 const port = 3000;
-var prompt = "";
+// var prompt = "";
 
 var mysql_con = mysql.createConnection({
 	host: "localhost",
@@ -31,7 +31,6 @@ mysql_con.connect(function(err){
 	if(err) throw err;
 	
 	console.log('Looking for table usrprofiles.');
-
 	let createUsrprofiles = "CREATE TABLE IF NOT EXISTS usrprofiles(profile_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,fname varchar(40) NOT NULL, lname varchar(40) NOT NULL, email varchar(40), contactnum bigint(20))";
 	mysql_con.query(createUsrprofiles, function(err, results, fields){
 		if(err){
@@ -39,7 +38,7 @@ mysql_con.connect(function(err){
 		}
 	});
 	
-        console.log('Looking for table usrlogin');
+    console.log('Looking for table usrlogin');
 	let createUsrlogin = "CREATE TABLE IF NOT EXISTS usrlogin(user_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,uname varchar(20) NOT NULL, pword varchar(20) NOT NULL, profile_id INT NOT NULL,FOREIGN KEY fk_usrlogin(profile_id) REFERENCES usrprofiles(profile_id))";
 	mysql_con.query(createUsrlogin, function(err, results, fields){
 		if(err){
@@ -47,7 +46,7 @@ mysql_con.connect(function(err){
 		} 
 	});
 
-	 console.log('Looking for table inventory.');
+	console.log('Looking for table inventory.');
 	// let createInventory = "CREATE TABLE IF NOT EXISTS inventory(inv_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,profile_id INT NOT NULL,FOREIGN KEY fk_inventory(profile_id) REFERENCES usrprofiles(profile_id),timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,i_quantity int(11),i_item varchar(45),	i_resistance varchar(45), i_RESwattage varchar(45),i_capacitance varchar(45),i_CAPtype varchar(45),i_CAPvoltage varchar(45),i_ICnum varchar(45),i_ICpackage varchar(45),i_LEDcolor varchar(45),	i_LEDsize varchar(45),i_MISCname varchar(100),i_remarks text)";
 	let createInventory = "CREATE TABLE IF NOT EXISTS inventory(inv_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,profile_id INT NOT NULL,FOREIGN KEY fk_inventory(profile_id) REFERENCES usrprofiles(profile_id),timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP, quantity int(11), item varchar(45), remarks text)";
 	mysql_con.query(createInventory, function(err, results, fields){
@@ -55,15 +54,24 @@ mysql_con.connect(function(err){
 			console.log(err.message);
 		}
 	});
-        console.log('Looking for table request.');
+
+    console.log('Looking for table request.');
 	// let createRequest =  "CREATE TABLE IF NOT EXISTS request(req_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,profile_id INT NOT NULL, FOREIGN KEY fk_request(profile_id) REFERENCES usrprofiles(profile_id),timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP, r_quantity int(11), r_item varchar(45),r_resistance varchar(45),r_RESwattage varchar(45),	r_capacitance varchar(45),r_CAPtype varchar(45),r_CAPvoltage varchar(45),r_ICnum varchar(45),r_ICpackage varchar(45),r_LEDcolor varchar(45),r_LEDsize varchar(45),r_MISCname varchar(100),r_remarks text)";
 	let createRequest =  "CREATE TABLE IF NOT EXISTS request(req_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,profile_id INT NOT NULL, FOREIGN KEY fk_request(profile_id) REFERENCES usrprofiles(profile_id),timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP, quantity int(11), item varchar(45), remarks text, batchname varchar(50))";
-	 mysql_con.query(createRequest, function(err, results, fields){
-                if(err){
-                        console.log(err.message);
-                }
-        });
-	
+	mysql_con.query(createRequest, function(err, results, fields){
+        if(err){
+            console.log(err.message);
+        }
+    });
+
+	console.log('Looking for table matches.');
+	let createMatches = "CREATE TABLE IF NOT EXISTS matches(match_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, inv_profile_id INT NOT NULL, FOREIGN KEY fk_inv_profile(inv_profile_id) REFERENCES usrprofiles(profile_id), inv_id INT NOT NULL, FOREIGN KEY fk_inv_matches(inv_id) REFERENCES inventory(inv_id), req_profile_id INT NOT NULL, FOREIGN KEY fk_req_profile(req_profile_id) REFERENCES usrprofiles(profile_id), req_id INT NOT NULL, FOREIGN KEY fk_req_matches(req_id) REFERENCES request(req_id))";
+	mysql_con.query(createMatches, function(err, results, fields) {
+		if(err) {
+			console.log(err.message);
+		}
+	})
+
 	console.log('Application running at 10.158.3.101:3000');
 });
 global.db = mysql_con;
@@ -76,14 +84,16 @@ app.use(session({
 	secret: 'big bad wolf',
 	resave: false,
 	saveUninitialized:true,
-	cookie: {maxAge: 60000}
+	cookie: {maxAge: null}
 }))
 	
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-	prompt = "";
-	res.render('pages/login')
+	var prompt = "";
+	res.render('pages/login', {
+		prompt : prompt
+	})
 })
 
 app.post('/login', function (req, res) {
@@ -92,20 +102,23 @@ app.post('/login', function (req, res) {
 	db.query(sql_com, function(err, result){
 		if(result.length){
 			sess.userId = result[0].profile_id;
-			sess.user = result[0];
-			console.log(result[0].profile_id);
+			sess.username = result[0].uname;
+			console.log(sess.username + " logged in");
 			res.redirect('/home')
 		}
 		else
 		{
-			message = 'Incorrect username or password!';
-			res.redirect('/');
+			var prompt = 'Incorrect username or password!';
+			res.render('pages/login', {
+				prompt : prompt
+			})
 		}
 	})
 })
 
 app.get('/home', function(req, res){
 	var userId = req.session.userId;
+	var username = req.session.username;
 	var sql_com_inventory = "SELECT * FROM inventory WHERE profile_id='"+userId+"'";
 	var sql_com_request = "SELECT * FROM request WHERE profile_id='"+userId+"'";
 	var sql_com_feed = "SELECT req_id, profile_id, timestamp, quantity, item, remarks FROM request UNION SELECT * FROM inventory ORDER BY timestamp ASC";
@@ -114,33 +127,28 @@ app.get('/home', function(req, res){
 	var request;
 	var feed;
 	mysql_con.query(sql_com_inventory, function(err, result, fields){
-		if (err)
-		{
+		if (err) {
 			inventory = [];
 		}
-		else
-		{
+		else {
 			inventory = result;
 		}
 		mysql_con.query(sql_com_request, function(err2, result2, fields2){
-			if (err) 
-			{
+			if (err) {
 				request = [];
 			}
-			else
-			{
+			else {
 				request = result2;
 			}
 			mysql_con.query(sql_com_feed, function(err3, result3, fields3){
-				if (err)
-				{
+				if (err) {
 					feed = [];
 				}
-				else
-				{
+				else {
 					feed = result3;
 				}
 				res.render('pages/home', {
+					username: username,
 					inventory: inventory,
 					request: request,
 					feed: feed
@@ -149,15 +157,16 @@ app.get('/home', function(req, res){
 		})
 	})
 })
-app.post('/logout', function(req, res){
+app.get('/logout', function(req, res){
 	var sess = req.session;
 	console.log('User logged out');
 	sess.destroy();
 	if(sess.user && sess.user_sid){
 		res.clearCookie('user_sid');
 		res.redirect('/');
-	}else{
-		re.redirect('/login');
+	}
+	else{
+		res.redirect('/');
 	}
 }); 
 app.post('/deleterequest', function(req, res){
@@ -195,6 +204,7 @@ app.post('/deleteinventory', function(req, res){
 
 
 app.get('/signup', function (req, res) {
+	var prompt = "";
 	res.render('pages/signup', {
 		prompt : prompt
 	})
@@ -207,15 +217,19 @@ app.post('/registered', function (req, res) {
 	var sql_com_usrlog = "INSERT INTO usrlogin (uname, pword, profile_id) VALUES (?, ?, ?)";
 	var prof_id;
 	if(!validator.isEmail(req.body.email)){
-		prompt = 'Email is invalid'
+		var prompt = 'Email is invalid'
 		console.log(prompt)
-		res.redirect('/signup')
+		res.render('pages/signup', {
+			prompt : prompt
+		})
 	}
 	else{
 		if(!validator.isMobilePhone(req.body.number)){
-			prompt = 'Mobile number is invalid'
+			var prompt = 'Mobile number is invalid'
 			console.log(prompt)
-			res.redirect('/signup')
+			res.render('pages/signup', {
+				prompt : prompt
+			})
 		}
 		else{
 			mysql_con.query(sql_com_usrnm,[req.body.username], function(err0, result0, fields0){
@@ -229,22 +243,22 @@ app.post('/registered', function (req, res) {
 							mysql_con.query(sql_com_usrlog, [req.body.username, req.body.password, prof_id], function(err3, result3){
 								if(err) throw err;
 								console.log("1 record inserted into usrlogin");
-								prompt = "";
 							});
 						});
 					});
 					res.redirect('/');
 				}
 				else{
-					prompt = "Username already taken"
+					var prompt = "Username already taken"
 					console.log(prompt)
-					res.redirect('/signup')
+					res.render('pages/signup', {
+						prompt : prompt
+					})
 				}
 			})
 		}
 	}
 })
-// })
 
 app.get('/addinv', function(req, res) {
 	res.render('pages/addinv');
@@ -254,9 +268,49 @@ app.get('/addreq', function(req, res) {
 	res.render('pages/addreq');
 })
 
-function insertComponent(req, table, userId){
+//The precious matching algorithm, ahahahahahaha so fun
+function matchingAlgorithm(compType, compDesc, otherTable, userId, curId) {
+	var sql_com_match = 'SELECT * FROM ' + otherTable + ' WHERE item = ? AND remarks = ? ORDER BY timestamp ASC';
+	var sql_com_values = [compType, compDesc];
+	var req_id;
+	var req_prof_id;
+	var inv_id;
+	var inv_prof_id;
+
+	db.query(sql_com_match, sql_com_values, function(err, result){
+		if(err) {
+			console.log("No match")
+		}
+		else {
+			if (result.length > 0) {
+				console.log("Found a match for " + userId + " with " + result[0].profile_id)
+				if (otherTable == 'request') {
+					req_id = result[0].req_id;
+					req_prof_id = result[0].profile_id;
+					inv_id = curId;
+					inv_prof_id = userId;
+				}
+				else {
+					req_id = curId;
+					req_prof_id = userId;
+					inv_id = result[0].inv_id;
+					inv_prof_id = result[0].profile_id;
+				}			
+				var sql_com_match_insert = 'INSERT INTO matches (inv_profile_id, inv_id, req_profile_id, req_id) VALUES (?, ?, ?, ?)';
+				var sql_com_match_values = [inv_prof_id, inv_id, req_prof_id, req_id];
+
+				db.query(sql_com_match_insert, sql_com_match_values, function(err2, result2) {
+					if (err) throw err;
+					console.log("1 record inserted into matches")
+				})
+			}
+		}
+	})
+}
+
+function insertComponent(req, table, otherTable, userId){
 	// var userId = req.session.userId;
-	console.log(userId)
+	// console.log(userId)
 	var quantity;
 	var number;
 	var compType;
@@ -297,7 +351,8 @@ function insertComponent(req, table, userId){
 			mysql_con.query(sql_com_addcomp, sql_com_values, function(err, result){
 				if (err) throw err;
 				log = "1 record inserted into " + table + " for " + userId;
-				console.log(log);
+				// console.log(result.insertId);
+				matchingAlgorithm(compType, compDesc, otherTable, userId, result.insertId)
 			})
 		}
 	}
@@ -305,13 +360,13 @@ function insertComponent(req, table, userId){
 
 app.post('/addreq', function(req, res) {
 	var userId = req.session.userId;
-	insertComponent(req, "request", userId)
+	insertComponent(req, "request", "inventory", userId)
 	res.redirect('/home')
 })
 
 app.post('/addinv', function(req, res) {
 	var userId = req.session.userId;
-	insertComponent(req, "inventory", userId)
+	insertComponent(req, "inventory", "request", userId)
 	res.redirect('/home') 
 })
 
